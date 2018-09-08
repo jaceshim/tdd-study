78aaec6f0522d67b6008278a7a7dff46c7279032
jaceshim <jaceshim@mcngamehub.com>
2018-09-08 22:49:08 +0900

    1. add

--- src/test/java/jace/shim/tdd/BowlingTest.java
+++ src/test/java/jace/shim/tdd/BowlingTest.java
@@ -1,5 +1,6 @@
 package jace.shim.tdd;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,8 +10,6 @@ public class BowlingTest {
 	@DisplayName("처음 roll(쓰러지는 pin개수) 를 실행하면 false를 리턴한다.")
 	void WhenFirstRollThenReturnTrue() {
 		Bowling bowling = new PlaygameBowling();
-		bowling.roll(1);
+		Assertions.assertThat(bowling.roll(1)).isTrue();
 	}
-
-
 }
