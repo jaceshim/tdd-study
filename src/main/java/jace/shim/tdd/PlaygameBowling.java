263cb2b4c38d683c386baba0d7f60193b3d99a6e
jaceshim <jaceshim@mcngamehub.com>
2018-09-08 23:14:24 +0900

    2. add

--- src/main/java/jace/shim/tdd/PlaygameBowling.java
+++ src/main/java/jace/shim/tdd/PlaygameBowling.java
@@ -3,6 +3,9 @@ package jace.shim.tdd;
 public class PlaygameBowling implements Bowling {
 	@Override
 	public boolean roll(int pin) {
+		if (pin < 0) {
+			throw new IllegalArgumentException();
+		}
 		return false;
 	}
 }
--- src/test/java/jace/shim/tdd/BowlingTest.java
+++ src/test/java/jace/shim/tdd/BowlingTest.java
@@ -1,8 +1,10 @@
 package jace.shim.tdd;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class BowlingTest {
 
@@ -10,6 +12,15 @@ public class BowlingTest {
 	@DisplayName("처음 roll(쓰러지는 pin개수) 를 실행하면 false를 리턴한다.")
 	void WhenFirstRollThenReturnTrue() {
 		Bowling bowling = new PlaygameBowling();
-		Assertions.assertThat(bowling.roll(1)).isTrue();
+		assertThat(bowling.roll(1)).isFalse();
+	}
+
+	@Test
+	@DisplayName("roll(쓰러지는 pin개수) 실행시 쓰러지는 pin개수가 0개 이하면 InvalidArgumentException 를 던진다.")
+	void When_ExecuteRollWithNegativeNumber_Then_ThrowsIllegalArgumentException() {
+		Bowling bowling = new PlaygameBowling();
+		assertThatThrownBy(() -> {
+			bowling.roll(-1);
+		}).isInstanceOf(IllegalArgumentException.class);
 	}
 }
